082347913126dc1a8e1b359e0ec11cee798fa668
Alberto Lacerda <albertoflfilho@gmail.com>
2023-12-19 15:47:32 +0000

    finishing `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ key: "works!" });
+  const updatedAt = new Date().toISOString();
+  const pgServerVersion = await database.query("SHOW server_version");
+  const pgMaxConnections = await database.query("SHOW max_connections");
+  const databaseName = process.env.POSTGRES_DB;
+  const pgOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const { server_version } = pgServerVersion.rows[0];
+  const { max_connections } = pgMaxConnections.rows[0];
+  const databaseOpenedConnectionsValue = pgOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        pg_server_version: Number(server_version),
+        pg_max_conections: Number(max_connections),
+        pg_opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,32 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  beforeEach(async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    return ({ updated_at, dependencies } = await response.json());
+  });
+
+  test("Should return status code 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+  });
+
+  test("Should return updated_at equal to parsedUpdatedAt valid ISO 8601", async () => {
+    expect(updated_at).toBeDefined();
+    const parsedUpdatedAt = new Date(updated_at).toISOString();
+    expect(updated_at).toEqual(parsedUpdatedAt);
+  });
+
+  test("Server version to return number", async () => {
+    expect(dependencies.database.pg_server_version).toBeDefined();
+    expect(dependencies.database.pg_server_version).toEqual(16.0);
+  });
+
+  test("Max connection to return number", async () => {
+    expect(dependencies.database.pg_max_conections).toBeDefined();
+    expect(dependencies.database.pg_max_conections).toEqual(100);
+  });
+
+  test("Opened connections to return number", async () => {
+    expect(dependencies.database.pg_opened_connections).toBeDefined();
+    expect(dependencies.database.pg_opened_connections).toEqual(1);
+  });
 });
