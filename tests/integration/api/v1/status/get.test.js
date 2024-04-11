fe9ef42d86e41d7afc8034144c82dc8d612cf0b5
Alberto Lacerda <albertoflfilho@gmail.com>
2024-04-11 18:50:48 +0000

    adds `/api/v1/migrations` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,32 +1,13 @@
-describe("GET to /api/v1/status", () => {
-  beforeEach(async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    return ({ updated_at, dependencies } = await response.json());
-  });
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 
-  test("Should return status code 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
-  });
+  const responseBody = await response.json();
 
-  test("Should return updated_at equal to parsedUpdatedAt valid ISO 8601", async () => {
-    expect(updated_at).toBeDefined();
-    const parsedUpdatedAt = new Date(updated_at).toISOString();
-    expect(updated_at).toEqual(parsedUpdatedAt);
-  });
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  test("Server version to return number", async () => {
-    expect(dependencies.database.pg_server_version).toBeDefined();
-    expect(dependencies.database.pg_server_version).toEqual("16.0");
-  });
-
-  test("Max connection to return number", async () => {
-    expect(dependencies.database.pg_max_conections).toBeDefined();
-    expect(dependencies.database.pg_max_conections).toEqual(112);
-  });
-
-  test("Opened connections to return number", async () => {
-    expect(dependencies.database.pg_opened_connections).toBeDefined();
-    expect(dependencies.database.pg_opened_connections).toEqual(1);
-  });
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
